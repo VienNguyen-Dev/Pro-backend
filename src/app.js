bd817d0c7d13c2071619324b76942319d426f855
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-07-02 09:20:44 +0700

    create route and controller file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js'
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))//thiết lập middleware để phân tích dữ liệu được gửi lên từ các form (được gửi qua phương thức POST) trong các yêu cầu HTTP.
 app.use(express.static("public"));
 app.use(cookieParser());
-
+app.use("/api/v1/users", userRouter);
 
 export { app }
 
